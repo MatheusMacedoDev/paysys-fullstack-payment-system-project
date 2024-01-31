626343081388063c00078446565248962de4d9d7
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-01-31 19:33:38 -0300

    Adding User Type to Domain

---
+++ paysys.webapi/Domain/Entities/UserType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace paysys.webapi.Domain.Entities;
+
+public class UserType
+{
+    [Key]
+    public Guid UserTypeId { get; private set; }
+
+    public String? TypeName { get; private set; }
+}
---
+++ paysys.webapi/Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using paysys.webapi.Domain.Entities;
+
+namespace paysys.webapi.Interfaces.Repositories;
+
+public interface IUserRepository
+{
+    void CreateUserType();
+    List<UserType> ListUserTypes();
+    UserType GetUserType();
+}
