0f05caccf8ec470d3e55ac84a2472b95a768c50a
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-01 22:07:44 -0300

    Creating access to database with current entity

---
+++ paysys.webapi/Infra/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using paysys.webapi.Domain.Entities;
+
+namespace paysys.webapi.Infra.Data;
+
+public class DataContext : DbContext
+{
+    public DbSet<UserType>? UserTypes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsetting.json")
+            .Build();
+
+        // Postgres Config
+        var connectionString = configuration.GetConnectionString("LocalConnection");
+        optionsBuilder.UseNpgsql(connectionString);
+
+        // ORM Config
+        optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    }
+}
--- paysys.webapi/Program.cs
+++ paysys.webapi/Program.cs
@@ -1,25 +1,26 @@
+using paysys.webapi.Infra.Data;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    // DbContext Injection
+    builder.Services.AddDbContext<DataContext>();
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
+    // Swagger Config
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
-app.UseAuthorization();
+    app.UseHttpsRedirection();
 
-app.MapControllers();
+    app.UseAuthorization();
+
+    app.MapControllers();
+}
 
 app.Run();
