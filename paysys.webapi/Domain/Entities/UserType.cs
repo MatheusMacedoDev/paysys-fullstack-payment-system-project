10df573fb9de1f671d7bbd1754a4fb134ba3cbed
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-02 20:50:06 -0300

    Adding table name to UserType

--- paysys.webapi/Domain/Entities/UserType.cs
+++ paysys.webapi/Domain/Entities/UserType.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace paysys.webapi.Domain.Entities;
 
+[Table("UserTypes")]
 public class UserType
 {
     [Key]
