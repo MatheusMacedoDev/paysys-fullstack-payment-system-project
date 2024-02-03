3ff271dd8639edae156f24247ad8647ddb869c43
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-02 21:29:14 -0300

    Creating a migration for generate UserTypes table

---
+++ paysys.webapi/Infra/Data/Migrations/20240203001924_CreatingUserTypesTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace paysys.webapi.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatingUserTypesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserTypes",
+                columns: table => new
+                {
+                    UserTypeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TypeName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTypes", x => x.UserTypeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserTypes");
+        }
+    }
+}
