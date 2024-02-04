81cbe02966e1ba29e7b0ea4f15a776a7e6ca56e6
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-04 00:33:36 -0300

    Adding a construct pattern with formatting

--- paysys.webapi/Domain/Entities/UserType.cs
+++ paysys.webapi/Domain/Entities/UserType.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
 
 namespace paysys.webapi.Domain.Entities;
 
@@ -10,4 +11,24 @@ public class UserType
     public Guid UserTypeId { get; private set; }
 
     public String? TypeName { get; private set; }
+
+    private UserType()
+    {
+    }
+
+    public static UserType Create(string typeName)
+    {
+        var userType = new UserType();
+
+        userType.UserTypeId = Guid.NewGuid();
+
+        userType.TypeName = FormatTypeName(typeName);
+
+        return userType;
+    }
+
+    private static string FormatTypeName(string name)
+    {
+        return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name);
+    }
 }
