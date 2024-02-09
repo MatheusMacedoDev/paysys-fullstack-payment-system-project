ee46e19f462f5490699144455611625f7e3258f4
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-08 21:26:44 -0300

    Creating contracts for create administrator use case

---
+++ paysys.webapi/Application/Contracts/Requests/CreateAdministratorRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace paysys.webapi.Application.Contracts.Requests;
+
+public record CreateAdministratorRequest(
+    string administratorName,
+    string cpf,
+    string userName,
+    string email,
+    string phoneNumber,
+    string password,
+    Guid userTypeId
+);
---
+++ paysys.webapi/Application/Contracts/Responses/CreateAdministratorResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace paysys.webapi.Application.Contracts.Responses;
+
+public record class CreateAdministratorResponse
+(
+    Guid administratorId,
+    Guid userId,
+    string administratorName
+);
