fce7182c9b89d57daa11c66047070cbeedf7161c
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-10 20:34:54 -0300

    Creating Users Controller

---
+++ paysys.webapi/Application/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using paysys.webapi.Application.Contracts.Requests;
+using paysys.webapi.Application.Services.UsersService;
+
+namespace paysys.webapi.Application.Controllers;
+
+[Route("api/users")]
+[ApiController]
+[Produces("application/json")]
+public class UsersController : ControllerBase
+{
+    private readonly IUsersService _usersService;
+
+    public UsersController(IUsersService usersService)
+    {
+        _usersService = usersService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAdministrator([FromBody] CreateAdministratorRequest request)
+    {
+        try
+        {
+            var response = await _usersService.CreateAdministrator(request);
+
+            return StatusCode(201, response);
+        }
+        catch (Exception error)
+        {
+            return BadRequest(error.Message);
+        }
+    }
+}
