313159cf8f6b34bbb5d0be1370889fddad2265e7
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-10 13:57:16 -0300

    Implementing cryptography in application

---
+++ paysys.webapi/Application/Strategies/Cryptography/ICryptographyStrategy.cs
@@ -0,0 +1,8 @@
+﻿namespace paysys.webapi.Application.Strategies.Cryptography;
+
+public interface ICryptographyStrategy
+{
+    byte[] MakeSalt();
+    byte[] MakeHashedPassword(string password, byte[] salt);
+    bool VerifyHashedPassword(string passwordInTest, byte[] passwordHash, byte[] passwordSalt);
+}
