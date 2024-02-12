25327db51d5c0fb708812d672d7092929ae438fa
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-12 11:43:44 -0300

    Creating contracts for create common user

---
+++ paysys.webapi/Application/Contracts/Requests/CreateCommonUserRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace paysys.webapi.Application.Contracts.Requests;
+
+public record CreateCommonUserRequest
+(
+    string commonUserName,
+    string cpf,
+    string userName,
+    string email,
+    string phoneNumber,
+    string password,
+    Guid userTypeId
+);
---
+++ paysys.webapi/Application/Contracts/Responses/CreateCommonUserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace paysys.webapi.Application.Contracts.Responses;
+
+public record CreateCommonUserResponse
+(
+    Guid commonUserId,
+    Guid userId,
+    string commonUserName
+);
