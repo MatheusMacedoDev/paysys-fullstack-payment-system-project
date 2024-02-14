eded4e0d22e87e4a69b2f99f62b2e6c212a80f19
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-13 22:17:52 -0300

    Creating contracts for shopkeepers creation

---
+++ paysys.webapi/Application/Contracts/Requests/CreateShopkeeperRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace paysys.webapi.Application.Contracts.Requests;
+
+public record CreateShopkeeperRequest(
+    string fancyName,
+    string companyName,
+    string cnpj,
+    string userName,
+    string email,
+    string phoneNumber,
+    string password,
+    Guid userTypeId
+);
+
---
+++ paysys.webapi/Application/Contracts/Responses/CreateShopkeeperResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace paysys.webapi.Application.Contracts.Responses;
+
+public record CreateShopkeeperResponse
+(
+    Guid shopkeeperId,
+    Guid userId,
+    string fancyName
+);
