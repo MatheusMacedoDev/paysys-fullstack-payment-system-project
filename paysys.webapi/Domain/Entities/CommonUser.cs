75893231f5493368c2f7191943e0ca9c20fb1c7b
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-03-06 20:42:21 -0300

    Renaming all common user table for match with naming conventions

--- paysys.webapi/Domain/Entities/CommonUser.cs
+++ paysys.webapi/Domain/Entities/CommonUser.cs
@@ -4,23 +4,26 @@ using Microsoft.EntityFrameworkCore;
 
 namespace paysys.webapi.Domain.Entities;
 
-[Table("CommonUsers")]
+[Table("common_users")]
 [Index(nameof(UserId), IsUnique = true)]
 public class CommonUser
 {
     [Key]
+    [Column("common_user_id")]
     public Guid CommonUserId { get; private set; }
 
     [Required]
+    [Column("common_user_name")]
     public string? CommonUserName { get; private set; }
 
     [Required]
-    [Column(TypeName = "CHAR(11)")]
+    [Column("common_user_cpf", TypeName = "CHAR(11)")]
     public string? CommonUserCPF { get; private set; }
 
     // User Reference
 
     [Required]
+    [Column("user_id")]
     public Guid UserId { get; private set; }
 
     [ForeignKey(nameof(UserId))]
---
+++ paysys.webapi/Infra/Data/Migrations/20240306234053_RenamingAllCommonUserTable.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace paysys.webapi.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamingAllCommonUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CommonUsers_Users_UserId",
+                table: "CommonUsers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CommonUsers",
+                table: "CommonUsers");
+
+            migrationBuilder.RenameTable(
+                name: "CommonUsers",
+                newName: "common_users");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "common_users",
+                newName: "user_id");
+
+            migrationBuilder.RenameColumn(
+                name: "CommonUserName",
+                table: "common_users",
+                newName: "common_user_name");
+
+            migrationBuilder.RenameColumn(
+                name: "CommonUserCPF",
+                table: "common_users",
+                newName: "common_user_cpf");
+
+            migrationBuilder.RenameColumn(
+                name: "CommonUserId",
+                table: "common_users",
+                newName: "common_user_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CommonUsers_UserId",
+                table: "common_users",
+                newName: "IX_common_users_user_id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_common_users",
+                table: "common_users",
+                column: "common_user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_common_users_Users_user_id",
+                table: "common_users",
+                column: "user_id",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_common_users_Users_user_id",
+                table: "common_users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_common_users",
+                table: "common_users");
+
+            migrationBuilder.RenameTable(
+                name: "common_users",
+                newName: "CommonUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "user_id",
+                table: "CommonUsers",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "common_user_name",
+                table: "CommonUsers",
+                newName: "CommonUserName");
+
+            migrationBuilder.RenameColumn(
+                name: "common_user_cpf",
+                table: "CommonUsers",
+                newName: "CommonUserCPF");
+
+            migrationBuilder.RenameColumn(
+                name: "common_user_id",
+                table: "CommonUsers",
+                newName: "CommonUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_common_users_user_id",
+                table: "CommonUsers",
+                newName: "IX_CommonUsers_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CommonUsers",
+                table: "CommonUsers",
+                column: "CommonUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CommonUsers_Users_UserId",
+                table: "CommonUsers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
