1e3879f9a60f4c2723d41c0ccf953862f8a442cd
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-03-17 17:30:03 -0300

    Changing the template reference for specification from user to user type

--- paysys.webapi/Domain/Specifications/IsNotAdministratorUserSpecification.cs
+++ paysys.webapi/Domain/Specifications/IsNotAdministratorUserSpecification.cs
@@ -1,32 +1,24 @@
 using Microsoft.Extensions.Options;
 using paysys.webapi.Configuration;
 using paysys.webapi.Domain.Entities;
-using paysys.webapi.Domain.Interfaces.Repositories;
 
 namespace paysys.webapi.Domain.Specifications;
 
-public class IsNotAdministratorUserSpecification : AsyncSpecification<User>
+public class IsNotAdministratorUserSpecification : Specification<UserType>
 {
     // Configurations
     private readonly string? AdministratorTypeName;
 
-    // Repositories
-    private readonly IUserTypesRepository _userTypesRepository;
-
-    public IsNotAdministratorUserSpecification(IOptions<UserTypeNamesSettings> settings, IUserTypesRepository userTypesRepository)
+    public IsNotAdministratorUserSpecification(IOptions<UserTypeNamesSettings> settings)
     {
         AdministratorTypeName = settings.Value.AdministratorTypeName;
-
-        _userTypesRepository = userTypesRepository;
     }
 
-    public async Task<bool> IsSatisfiedBy(User user)
+    public bool IsSatisfiedBy(UserType type)
     {
         try
         {
-            UserType userType = await _userTypesRepository.GetUserType(user.UserTypeId)!;
-
-            if (userType.TypeName != AdministratorTypeName)
+            if (type.TypeName != AdministratorTypeName)
             {
                 return true;
             }
