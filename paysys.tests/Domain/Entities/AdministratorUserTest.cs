45e8f552ee2ea4695052b037cb6c6c99bdb31d06
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-03-27 16:14:56 -0300

    Creating administrator user initial tests

---
+++ paysys.tests/Domain/Entities/AdministratorUserTest.cs
@@ -0,0 +1,58 @@
+using paysys.webapi.Domain.Entities;
+
+namespace paysys.tests.Domain.Entities;
+
+public class AdministratorUserTest
+{
+    [Fact]
+    public void CreateAdministratorUserCorrectly()
+    {
+        var administrator = new AdministratorUser(
+            administratorName: "Matheus Macedo Santos",
+            administratorCPF: "67778379847",
+            userId: Guid.Empty
+        );
+
+        if (!administrator.IsValid)
+        {
+            foreach (var notification in administrator.Notifications)
+            {
+                Console.WriteLine(notification.Message);
+            }
+        }
+
+        Assert.True(administrator.IsValid);
+    }
+
+    [Fact]
+    public void CreateAdministratorUserWithIncorrectName()
+    {
+        var administrator = new AdministratorUser(
+            administratorName: "Matheus Macedo santos",
+            administratorCPF: "67778379847",
+            userId: Guid.Empty
+        );
+
+        var isAdministratorNameInvalid = IsUserPropertyInvalid(administrator, "AdministratorName");
+
+        Assert.True(isAdministratorNameInvalid);
+        Assert.False(administrator.IsValid);
+    }
+
+    private bool IsUserPropertyInvalid(AdministratorUser administrator, string propertyName)
+    {
+        if (!administrator.IsValid)
+        {
+            foreach (var notification in administrator.Notifications)
+            {
+                if (notification.Key == propertyName)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+    }
+}
