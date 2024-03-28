4aefd06b8410eef83a9db1eb15ad7cf1d7902f0b
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-03-28 15:43:09 -0300

    Creating validations for user type entity

--- paysys.webapi/Domain/Entities/UserType.cs
+++ paysys.webapi/Domain/Entities/UserType.cs
@@ -1,13 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Globalization;
+using Flunt.Notifications;
+using Flunt.Validations;
 using Microsoft.EntityFrameworkCore;
 
 namespace paysys.webapi.Domain.Entities;
 
 [Table("user_types")]
 [Index(nameof(TypeName), IsUnique = true)]
-public class UserType
+public class UserType : Notifiable<Notification>
 {
     [Key]
     [Column("user_type_id")]
@@ -16,28 +18,27 @@ public class UserType
     [Column("user_type_name")]
     public String? TypeName { get; private set; }
 
-    private UserType()
+    public UserType(string typeName)
     {
+        UserTypeId = Guid.NewGuid();
+
+        ChangeTypeName(typeName);
     }
 
-    public static UserType Create(string typeName)
-    {
-        var userType = new UserType();
-
-        userType.UserTypeId = Guid.NewGuid();
-
-        userType.TypeName = FormatTypeName(typeName);
-
-        return userType;
-    }
-
-    public void SetTypeName(string newTypeName)
+    public void ChangeTypeName(string newTypeName)
     {
         var formatedTypeName = FormatTypeName(newTypeName);
+
+        AddNotifications(new Contract<UserType>()
+            .IsNotNullOrEmpty(formatedTypeName, "TypeName", "O nome do tipo de usuário não pode ser nulo ou vazio")
+            .IsGreaterOrEqualsThan(formatedTypeName, 4, "TypeName", "O nome do tipo de usuário deve conter mais fe quatro caracteres")
+            .Matches(formatedTypeName, @"^(\s?[A-Z][a-z]+\s?)+$", "TypeName", "Nome de tipo de usuário inválido")
+        );
+
         TypeName = formatedTypeName;
     }
 
-    private static string FormatTypeName(string name)
+    private string FormatTypeName(string name)
     {
         var lowerCaseName = name.ToLower();
         var pascalCaseName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(lowerCaseName);
