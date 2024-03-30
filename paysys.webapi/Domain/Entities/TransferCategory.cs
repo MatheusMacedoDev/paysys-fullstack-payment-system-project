5f22f075f4d665f3ed071977a14ed585186826d9
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-03-29 23:03:37 -0300

    Creating validations for transfer category entity

--- paysys.webapi/Domain/Entities/TransferCategory.cs
+++ paysys.webapi/Domain/Entities/TransferCategory.cs
@@ -1,12 +1,14 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Flunt.Notifications;
+using Flunt.Validations;
 using Microsoft.EntityFrameworkCore;
 
 namespace paysys.webapi.Domain.Entities;
 
 [Table("transfer_categories")]
 [Index(nameof(TransferCategoryName), IsUnique = true)]
-public class TransferCategory
+public class TransferCategory : Notifiable<Notification>
 {
     [Key]
     [Column("transfer_category_id")]
@@ -16,17 +18,23 @@ public class TransferCategory
     [Column("transfer_category_name")]
     public string? TransferCategoryName { get; private set; }
 
-    private TransferCategory()
+    public TransferCategory(string transferCategoryName)
     {
+        TransferCategoryId = Guid.NewGuid();
+
+        ChangeTransferCategoryName(transferCategoryName);
     }
 
-    public static TransferCategory Create(string categoryName)
+    private void ChangeTransferCategoryName(string categoryName)
     {
-        var category = new TransferCategory();
+        categoryName = categoryName.Trim();
 
-        category.TransferCategoryId = Guid.NewGuid();
-        category.TransferCategoryName = categoryName;
+        AddNotifications(new Contract<TransferStatus>()
+            .IsNotNullOrEmpty(categoryName, "TransferCategoryName", "O nome da categoria da transferência não deve ser nulo ou vazio")
+            .IsGreaterOrEqualsThan(categoryName, 4, "TransferCategoryName", "O nome da categoria da transferência deve conter mais de quatro caracteres")
+            .Matches(categoryName, @"^(\s?[A-Z][a-z]+\s?)+$", "TransferCategoryName", "Nome da categoria da transferência inválido")
+        );
 
-        return category;
+        TransferCategoryName = categoryName;
     }
 }
