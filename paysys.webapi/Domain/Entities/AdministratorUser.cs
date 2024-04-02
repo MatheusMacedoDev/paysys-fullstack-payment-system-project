733f7bd89fb708cb61c0cf8237b8ee882ec658d6
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-02 13:55:59 -0300

    Replacing administrator CPF string for a CPF value object

--- paysys.webapi/Domain/Entities/AdministratorUser.cs
+++ paysys.webapi/Domain/Entities/AdministratorUser.cs
@@ -1,7 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Flunt.Notifications;
-using Flunt.Validations;
 using Microsoft.EntityFrameworkCore;
 using paysys.webapi.Domain.ValueObjects;
 
@@ -9,18 +7,14 @@ namespace paysys.webapi.Domain.Entities;
 
 [Table("administrator_users")]
 [Index(nameof(UserId), IsUnique = true)]
-[Index(nameof(AdministratorCPF), IsUnique = true)]
-public class AdministratorUser : Notifiable<Notification>
+public class AdministratorUser
 {
     [Key]
     [Column("administrator_id")]
     public Guid AdministratorId { get; private set; }
 
     public Name? AdministratorName { get; private set; }
-
-    [Required]
-    [Column("administrator_cpf", TypeName = "CHAR(11)")]
-    public string? AdministratorCPF { get; private set; }
+    public CPF? AdministratorCPF { get; private set; }
 
     // User Reference
 
@@ -38,21 +32,10 @@ public class AdministratorUser : Notifiable<Notification>
     public AdministratorUser(string administratorName, string administratorCPF, Guid userId)
     {
         AdministratorId = Guid.NewGuid();
+
         AdministratorName = new Name(administratorName);
-        ChangeAdministratorCPF(administratorCPF);
+        AdministratorCPF = new CPF(administratorCPF);
 
         UserId = userId;
-    }
-
-    private void ChangeAdministratorCPF(string administratorCPF)
-    {
-        administratorCPF = administratorCPF.Trim();
-
-        AddNotifications(new Contract<AdministratorUser>()
-            .IsNotNullOrEmpty(administratorCPF, "AdministratorCPF", "O CPF do administrador não deve ser nulo ou vazio")
-            .Matches(administratorCPF, @"^\d{11}$", "AdministratorCPF", "O CPF conforme descrito é inválido")
-        );
-
-        AdministratorCPF = administratorCPF;
     }
 }
