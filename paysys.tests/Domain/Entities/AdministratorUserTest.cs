83bafa99c58a32eb5583ddcd9a238711a790b501
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-02 14:44:00 -0300

    Fixing the administrator user test to accept changes related to increase of CPF value object

--- paysys.tests/Domain/Entities/AdministratorUserTest.cs
+++ paysys.tests/Domain/Entities/AdministratorUserTest.cs
@@ -13,46 +13,7 @@ public class AdministratorUserTest
             userId: Guid.Empty
         );
 
-        if (!administrator.IsValid)
-        {
-            foreach (var notification in administrator.Notifications)
-            {
-                Console.WriteLine(notification.Message);
-            }
-        }
-
-        Assert.True(administrator.IsValid);
-    }
-
-    [Fact]
-    public void CreateAdministratorUserWithInvalidCPF()
-    {
-        var administrator = new AdministratorUser(
-            administratorName: "Matheus Macedo Santos",
-            administratorCPF: "6777837984e",
-            userId: Guid.Empty
-        );
-
-        var isCPFInvalid = IsUserPropertyInvalid(administrator, "AdministratorCPF");
-
-        Assert.True(isCPFInvalid);
-        Assert.False(administrator.IsValid);
-    }
-
-    private bool IsUserPropertyInvalid(AdministratorUser administrator, string propertyName)
-    {
-        if (!administrator.IsValid)
-        {
-            foreach (var notification in administrator.Notifications)
-            {
-                if (notification.Key == propertyName)
-                {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-
+        Assert.True(administrator.AdministratorName!.IsValid);
+        Assert.True(administrator.AdministratorCPF!.IsValid);
     }
 }
