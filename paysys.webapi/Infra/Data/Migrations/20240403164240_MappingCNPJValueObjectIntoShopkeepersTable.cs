2f6ef3c3ea3c0f0da4b49a2d0b8afa69a7ca7f5a
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-03 13:50:57 -0300

    Creating migration for apply changes maintaining CNPJ index

---
+++ paysys.webapi/Infra/Data/Migrations/20240403164240_MappingCNPJValueObjectIntoShopkeepersTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace paysys.webapi.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MappingCNPJValueObjectIntoShopkeepersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "shopkeeper_cnpj",
+                table: "shopkeepers",
+                type: "CHAR(14)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "CHAR(14)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "shopkeeper_cnpj",
+                table: "shopkeepers",
+                type: "CHAR(14)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "CHAR(14)",
+                oldNullable: true);
+        }
+    }
+}
