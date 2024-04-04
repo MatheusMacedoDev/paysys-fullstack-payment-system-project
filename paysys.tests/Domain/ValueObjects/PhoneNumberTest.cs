8140742afeb1ddadfa59796a93335f575d3b6193
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-04 14:06:22 -0300

    Creating a test for PhoneNumber value object

---
+++ paysys.tests/Domain/ValueObjects/PhoneNumberTest.cs
@@ -0,0 +1,30 @@
+using paysys.webapi.Domain.ValueObjects;
+
+namespace paysys.tests.Domain.ValueObjects;
+
+public class PhoneNumberTest
+{
+    [Fact]
+    public void CreateValidPhoneNumber()
+    {
+        PhoneNumber testedPhoneNumber = new PhoneNumber("11984236577");
+
+        if (!testedPhoneNumber.IsValid)
+        {
+            foreach (var notification in testedPhoneNumber.Notifications)
+            {
+                Console.WriteLine(notification.Message);
+            }
+        }
+
+        Assert.True(testedPhoneNumber.IsValid);
+    }
+
+    [Fact]
+    public void CreateInvalidPhoneNumber()
+    {
+        Action actual = () => new PhoneNumber("1198423657A");
+
+        Assert.Throws<ArgumentException>(actual);
+    }
+}
