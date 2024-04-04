4bb8f6e1e59a69c067d91f4120d2760ba91df45f
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-04 14:17:33 -0300

    Creating PhoneNumber value object

---
+++ paysys.webapi/Domain/ValueObjects/PhoneNumber.cs
@@ -0,0 +1,33 @@
+using Flunt.Validations;
+using paysys.webapi.Utils;
+
+namespace paysys.webapi.Domain.ValueObjects;
+
+public class PhoneNumber : ValueObject
+{
+    public string? PhoneNumberText { get; init; }
+
+    protected PhoneNumber()
+    {
+    }
+
+    public PhoneNumber(string phoneNumberText)
+    {
+        phoneNumberText = StringFormatter.FullyClear(phoneNumberText);
+
+        AddNotifications(new Contract<PhoneNumber>()
+            .IsNotNullOrEmpty(phoneNumberText, "PhoneNumber", "O número de telefone não pode ser nulo ou vazio")
+            .Matches(phoneNumberText, "^[0-9]{11}*$", "PhoneNumber", "O número de telefone é inválido")
+        );
+
+        if (!IsValid)
+            throw new ArgumentException("Número de telefone inválido");
+
+        PhoneNumberText = phoneNumberText;
+    }
+
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        yield return PhoneNumberText!;
+    }
+}
