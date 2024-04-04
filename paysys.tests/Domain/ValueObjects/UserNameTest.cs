e9ef80865c344eab8cb39c060d97cdce229fe24d
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-04 14:03:25 -0300

    Creating a test for UserName value object

---
+++ paysys.tests/Domain/ValueObjects/UserNameTest.cs
@@ -0,0 +1,30 @@
+using paysys.webapi.Domain.ValueObjects;
+
+namespace paysys.tests.Domain.ValueObjects;
+
+public class UserNameTest
+{
+    [Fact]
+    public void CreateValidUserName()
+    {
+        UserName testedUserName = new UserName("Mathe1");
+
+        if (!testedUserName.IsValid)
+        {
+            foreach (var notification in testedUserName.Notifications)
+            {
+                Console.WriteLine(notification.Message);
+            }
+        }
+
+        Assert.True(testedUserName.IsValid);
+    }
+
+    [Fact]
+    public void CreateInvalidName()
+    {
+        Action actual = () => new UserName("009487");
+
+        Assert.Throws<ArgumentException>(actual);
+    }
+}
