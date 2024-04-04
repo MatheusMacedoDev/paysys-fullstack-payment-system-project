5b1402e2b7b63a9ae80a78f8e15d320730b48904
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-04 20:14:06 -0300

    Applying value objects to Shopkeeper

--- paysys.webapi/Domain/Entities/Shopkeeper.cs
+++ paysys.webapi/Domain/Entities/Shopkeeper.cs
@@ -1,28 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Flunt.Notifications;
-using Flunt.Validations;
 using Microsoft.EntityFrameworkCore;
 using paysys.webapi.Domain.ValueObjects;
-using paysys.webapi.Utils;
 
 namespace paysys.webapi.Domain.Entities;
 
 [Table("shopkeepers")]
 [Index(nameof(UserId), IsUnique = true)]
-public class Shopkeeper : Notifiable<Notification>
+public class Shopkeeper
 {
     [Key]
     [Column("shopkeeper_id")]
     public Guid ShopkeeperId { get; private set; }
 
-    [Required]
-    [Column("fancy_name")]
-    public string? FancyName { get; private set; }
-
-    [Required]
-    [Column("company_name")]
-    public string? CompanyName { get; private set; }
+    public CorporateName? FancyName { get; private set; }
+    public CorporateName? CompanyName { get; private set; }
 
     public CNPJ? ShopkeeperCNJP { get; private set; }
 
@@ -47,40 +39,13 @@ public class Shopkeeper : Notifiable<Notification>
     {
         ShopkeeperId = Guid.NewGuid();
 
-        ChangeFancyName(fancyName);
-        ChangeCompanyName(companyName);
-
+        FancyName = new CorporateName(fancyName);
+        CompanyName = new CorporateName(companyName);
         ShopkeeperCNJP = new CNPJ(shopkeeperCNJP);
 
         Balance = 0;
 
         UserId = userId;
-    }
-
-    private void ChangeFancyName(string fancyName)
-    {
-        fancyName = StringFormatter.BasicClear(fancyName);
-
-        AddNotifications(new Contract<Shopkeeper>()
-            .IsNotNullOrEmpty(fancyName, "FancyName", "O nome fantasia não pode ser nulo ou vazio")
-            .Matches(fancyName, @"^(\s?[A-Z][a-zA-Z]+\s?)+$", "FancyName", "Nome fantasia inválido")
-        );
-
-        FancyName = fancyName;
-    }
-
-    private void ChangeCompanyName(string companyName)
-    {
-        companyName = StringFormatter.BasicClear(companyName);
-
-        AddNotifications(new Contract<Shopkeeper>()
-            .IsNotNullOrEmpty(companyName, "CompanyName", "A razão social não pode ser nula ou vazia")
-            .IsGreaterOrEqualsThan(companyName, 10, "CompanyName", "A razão social não pode ter menos que dez caracteres")
-            .IsLowerOrEqualsThan(companyName, 115, "CompanyName", "A razão social não pode exceder 115 caracteres")
-            .Matches(companyName, @"^(\s?[A-Z][a-zA-Z]+\s?)+$", "CompanyName", "Razão social inválida")
-        );
-
-        CompanyName = companyName;
     }
 
     public void IncreaseMoney(double amount)
