b8d615862117315ce2ec57c9082043318cf0b010
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-04 20:16:14 -0300

    Fixing ShopkeeperTest

--- paysys.tests/Domain/Entities/ShopkeeperTest.cs
+++ paysys.tests/Domain/Entities/ShopkeeperTest.cs
@@ -14,63 +14,8 @@ public class ShopkeeperTest
             userId: Guid.Empty
         );
 
-        if (!shopkeeper.IsValid)
-        {
-            foreach (var notification in shopkeeper.Notifications)
-            {
-                Console.WriteLine(notification.Message);
-            }
-        }
-
-        Assert.True(shopkeeper.IsValid);
-    }
-
-    [Fact]
-    public void CreateShopkeeperWithIncorrectFancyName()
-    {
-        var shopkeeper = new Shopkeeper(
-            fancyName: "Contabilizei empresa",
-            companyName: "Contabilizei Contabilidade LTDA",
-            shopkeeperCNJP: "12345678912345",
-            userId: Guid.Empty
-        );
-
-        var isFancyNameInvalid = IsUserPropertyInvalid(shopkeeper, "FancyName");
-
-        Assert.True(isFancyNameInvalid);
-        Assert.False(shopkeeper.IsValid);
-    }
-
-    [Fact]
-    public void CreateShopkeeperWithIncorrectCompanyName()
-    {
-        var shopkeeper = new Shopkeeper(
-            fancyName: "Contabilizei empresa",
-            companyName: "Contabilizei Contabilidade lTDA",
-            shopkeeperCNJP: "12345678912345",
-            userId: Guid.Empty
-        );
-
-        var isCompanyNameInvalid = IsUserPropertyInvalid(shopkeeper, "CompanyName");
-
-        Assert.True(isCompanyNameInvalid);
-        Assert.False(shopkeeper.IsValid);
-    }
-
-    private bool IsUserPropertyInvalid(Shopkeeper shopkeeper, string propertyName)
-    {
-        if (!shopkeeper.IsValid)
-        {
-            foreach (var notification in shopkeeper.Notifications)
-            {
-                if (notification.Key == propertyName)
-                {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-
+        Assert.True(shopkeeper.FancyName!.IsValid);
+        Assert.True(shopkeeper.CompanyName!.IsValid);
+        Assert.True(shopkeeper.ShopkeeperCNJP!.IsValid);
     }
 }
