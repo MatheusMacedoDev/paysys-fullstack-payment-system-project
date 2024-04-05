bcb08caabce3aa47bc151c1cef5441f7976fea88
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-04 21:43:08 -0300

    Updating transfer category test to new changes

--- paysys.tests/Domain/Entities/TransferCategoryTest.cs
+++ paysys.tests/Domain/Entities/TransferCategoryTest.cs
@@ -11,44 +11,6 @@ public class TransferCategoryTest
             transferCategoryName: "Alimentos"
         );
 
-        if (!category.IsValid)
-        {
-            foreach (var notification in category.Notifications)
-            {
-                Console.WriteLine(notification.Message);
-            }
-        }
-
-        Assert.True(category.IsValid);
-    }
-
-    [Fact]
-    public void CreateTransferCategoryWithIncorrectCategoryName()
-    {
-        var category = new TransferCategory(
-            transferCategoryName: "Alim3ntos"
-        );
-
-        var isCategoryNameInvalid = IsCategoryPropertyInvalid(category, "TransferCategoryName");
-
-        Assert.True(isCategoryNameInvalid);
-        Assert.False(category.IsValid);
-    }
-
-    private bool IsCategoryPropertyInvalid(TransferCategory category, string propertyName)
-    {
-        if (!category.IsValid)
-        {
-            foreach (var notification in category.Notifications)
-            {
-                if (notification.Key == propertyName)
-                {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-
+        Assert.True(category.TransferCategoryName!.IsValid);
     }
 }
