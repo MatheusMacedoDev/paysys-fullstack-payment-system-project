2f30b3b2b0db66f9a0d7c0e55b1f395afec482a8
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-04 21:47:00 -0300

    Adding Name value object to TransferStatus entity

--- paysys.webapi/Domain/Entities/TransferStatus.cs
+++ paysys.webapi/Domain/Entities/TransferStatus.cs
@@ -1,41 +1,29 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Flunt.Notifications;
-using Flunt.Validations;
-using Microsoft.EntityFrameworkCore;
-using paysys.webapi.Utils;
+using paysys.webapi.Domain.ValueObjects;
 
 namespace paysys.webapi.Domain.Entities;
 
 [Table("transfer_status")]
-[Index(nameof(TransferStatusName), IsUnique = true)]
-public class TransferStatus : Notifiable<Notification>
+public class TransferStatus
 {
     [Key]
     [Column("transfer_status_id")]
     public Guid TransferStatusId { get; private set; }
 
-    [Required]
-    [Column("transfer_status_name")]
-    public string? TransferStatusName { get; private set; }
+    public Name? TransferStatusName { get; private set; }
+
+    protected TransferStatus()
+    {
+    }
 
     public TransferStatus(string transferStatusName)
     {
         TransferStatusId = Guid.NewGuid();
 
-        ChangeTransferStatusName(transferStatusName);
-    }
-
-    private void ChangeTransferStatusName(string statusName)
-    {
-        statusName = StringFormatter.BasicClear(statusName);
-
-        AddNotifications(new Contract<TransferStatus>()
-            .IsNotNullOrEmpty(statusName, "TransferStatusName", "O nome do status da transferência não deve ser nulo ou vazio")
-            .IsGreaterOrEqualsThan(statusName, 4, "TransferStatusName", "O nome do status da transferência deve conter mais de quatro caracteres")
-            .Matches(statusName, @"^(\s?[A-Z][a-z]+\s?)+$", "TransferStatusName", "Nome do status da transferência inválido")
+        TransferStatusName = new Name(
+            nameText: transferStatusName,
+            maxCharacters: 4
         );
-
-        TransferStatusName = statusName;
     }
 }
