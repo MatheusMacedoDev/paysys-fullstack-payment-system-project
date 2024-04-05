01174e46d15dbf1fb72929f2409b718c6bea31e9
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-04 21:29:18 -0300

    Replacing the transfer category name string for a configured Name value object

--- paysys.webapi/Domain/Entities/TransferCategory.cs
+++ paysys.webapi/Domain/Entities/TransferCategory.cs
@@ -1,41 +1,25 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Flunt.Notifications;
-using Flunt.Validations;
-using Microsoft.EntityFrameworkCore;
-using paysys.webapi.Utils;
+using paysys.webapi.Domain.ValueObjects;
 
 namespace paysys.webapi.Domain.Entities;
 
 [Table("transfer_categories")]
-[Index(nameof(TransferCategoryName), IsUnique = true)]
-public class TransferCategory : Notifiable<Notification>
+public class TransferCategory
 {
     [Key]
     [Column("transfer_category_id")]
     public Guid TransferCategoryId { get; set; }
 
-    [Required]
-    [Column("transfer_category_name")]
-    public string? TransferCategoryName { get; private set; }
+    public Name? TransferCategoryName { get; private set; }
 
     public TransferCategory(string transferCategoryName)
     {
         TransferCategoryId = Guid.NewGuid();
 
-        ChangeTransferCategoryName(transferCategoryName);
-    }
-
-    private void ChangeTransferCategoryName(string categoryName)
-    {
-        categoryName = StringFormatter.BasicClear(categoryName);
-
-        AddNotifications(new Contract<TransferStatus>()
-            .IsNotNullOrEmpty(categoryName, "TransferCategoryName", "O nome da categoria da transferência não deve ser nulo ou vazio")
-            .IsGreaterOrEqualsThan(categoryName, 4, "TransferCategoryName", "O nome da categoria da transferência deve conter mais de quatro caracteres")
-            .Matches(categoryName, @"^(\s?[A-Z][a-z]+\s?)+$", "TransferCategoryName", "Nome da categoria da transferência inválido")
+        TransferCategoryName = new Name(
+            nameText: transferCategoryName,
+            maxCharacters: 4
         );
-
-        TransferCategoryName = categoryName;
     }
 }
