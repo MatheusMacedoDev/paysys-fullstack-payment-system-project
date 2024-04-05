7f012d14fa12803662e6c7a9e4e06fa9132eb762
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-05 13:11:30 -0300

    Adding remaining value objects to replace some properties into User entity

--- paysys.webapi/Domain/Entities/User.cs
+++ paysys.webapi/Domain/Entities/User.cs
@@ -1,35 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Flunt.Notifications;
-using Flunt.Validations;
-using Microsoft.EntityFrameworkCore;
 using paysys.webapi.Application.Strategies.Cryptography;
 using paysys.webapi.Domain.ValueObjects;
-using paysys.webapi.Utils;
 
 namespace paysys.webapi.Domain.Entities;
 
 [Table("users")]
-[Index(nameof(UserName), IsUnique = true)]
-[Index(nameof(Email), IsUnique = true)]
-public class User : Notifiable<Notification>
+public class User
 {
     [Key]
     [Column("user_id")]
     public Guid UserId { get; private set; }
 
-    [Required]
-    [Column("user_name")]
-    public string? UserName { get; private set; }
-
-    [Required]
-    [Column("email")]
-    public string? Email { get; private set; }
-
-    [Required]
-    [Column("phone_number", TypeName = "CHAR(11)")]
-    public string? PhoneNumber { get; private set; }
-
+    public UserName? UserName { get; private set; }
+    public Email? Email { get; private set; }
+    public PhoneNumber? PhoneNumber { get; private set; }
     public Password? Password { get; private set; }
 
     [Required]
@@ -57,10 +42,9 @@ public class User : Notifiable<Notification>
     {
         UserId = Guid.NewGuid();
 
-        ChangeUserName(userName);
-        ChangeEmail(email);
-        ChangePhoneNumber(phoneNumber);
-
+        UserName = new UserName(userName);
+        Email = new Email(email);
+        PhoneNumber = new PhoneNumber(phoneNumber);
         Password = new Password(password, cryptographyStrategy);
 
         var currentDateTime = DateTime.UtcNow;
@@ -69,46 +53,5 @@ public class User : Notifiable<Notification>
         LastUpdatedOn = currentDateTime;
 
         UserTypeId = userTypeId;
-    }
-
-    private void ChangeUserName(string userName)
-    {
-        userName = StringFormatter.BasicClear(userName);
-
-        AddNotifications(new Contract<User>()
-            .IsNotNullOrEmpty(userName, "UserName", "O nome de usuário não deve ser nulo ou vazio")
-            .IsGreaterThan(userName, 5, "UserName", "O nome de usuário deve conter mais de cinco carácteres")
-            .Matches(userName, "[a-zA-Z]+", "UserName", "O nome de usuário deve conter letras")
-        );
-
-        if (IsValid)
-            UserName = userName;
-    }
-
-    private void ChangeEmail(string email)
-    {
-        email = StringFormatter.BasicClear(email);
-
-        AddNotifications(new Contract<User>()
-            .IsNotNullOrEmpty(email, "Email", "O e-mail não deve ser nulo ou vazio")
-            .IsEmail(email, "Email", "O e-mail digitado não é válido")
-        );
-
-        if (IsValid)
-            Email = email;
-    }
-
-    private void ChangePhoneNumber(string phoneNumber)
-    {
-        phoneNumber = StringFormatter.FullyClear(phoneNumber);
-
-        AddNotifications(new Contract<User>()
-            .IsNotNullOrEmpty(phoneNumber, "PhoneNumber", "O número de telefone não pode ser nulo ou vazio")
-            .IsGreaterOrEqualsThan(phoneNumber, 11, "PhoneNumber", "O número de telefone deve conter onze dígitos ao todo")
-            .IsLowerOrEqualsThan(phoneNumber, 11, "PhoneNumber", "O número de telefone deve conter onze dígitos ao todo")
-            .Matches(phoneNumber, "^[0-9]*$", "PhoneNumber", "O número de telefone é inválido")
-        );
-
-        PhoneNumber = phoneNumber;
     }
 }
