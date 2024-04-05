19f2ab18321ab483308099f933790941a73b5619
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-05 13:29:32 -0300

    Mapping last value objects into User table

--- paysys.webapi/Infra/Data/DbConfiguration/UserConfiguration.cs
+++ paysys.webapi/Infra/Data/DbConfiguration/UserConfiguration.cs
@@ -8,16 +8,42 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
     {
-        builder.OwnsOne(e => e.Password)
-            .Property(p => p.Hash)
+        builder.OwnsOne(user => user.UserName, ownedBuilder =>
+        {
+            ownedBuilder.Property(userName => userName.NameText)
+                .HasColumnName("user_name")
+                .IsRequired();
+
+            ownedBuilder.HasIndex(userName => userName.NameText)
+                .IsUnique();
+        });
+
+        builder.OwnsOne(user => user.Email, ownedBuilder =>
+        {
+            ownedBuilder.Property(email => email.EmailText)
+                .HasColumnName("email")
+                .IsRequired();
+
+            ownedBuilder.HasIndex(email => email.EmailText)
+                .IsUnique();
+        });
+
+        builder.OwnsOne(user => user.PhoneNumber)
+            .Property(phoneNumber => phoneNumber.PhoneNumberText)
+            .HasColumnName("phone_number")
+            .HasColumnType("CHAR(11)")
+            .IsRequired();
+
+        builder.OwnsOne(user => user.Password)
+            .Property(password => password.Hash)
             .HasColumnName("password_hash")
             .HasColumnType("BYTEA")
-            .IsRequired(true);
+            .IsRequired();
 
-        builder.OwnsOne(e => e.Password)
-            .Property(p => p.Salt)
+        builder.OwnsOne(user => user.Password)
+            .Property(password => password.Salt)
             .HasColumnName("password_salt")
             .HasColumnType("BYTEA")
-            .IsRequired(true);
+            .IsRequired();
     }
 }
---
+++ paysys.webapi/Infra/Data/Migrations/20240405162814_MappingLastValueObjectsIntoUserTable.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace paysys.webapi.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MappingLastValueObjectsIntoUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "user_name",
+                table: "users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phone_number",
+                table: "users",
+                type: "CHAR(11)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "CHAR(11)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "user_name",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phone_number",
+                table: "users",
+                type: "CHAR(11)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "CHAR(11)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
