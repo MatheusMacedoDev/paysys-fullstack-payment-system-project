b55d6f03d4d89ef5960a6404331bfecf012d998b
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-08 14:36:39 -0300

    Creating a test for get formatted phone number

--- paysys.tests/Domain/ValueObjects/PhoneNumberTest.cs
+++ paysys.tests/Domain/ValueObjects/PhoneNumberTest.cs
@@ -27,4 +27,15 @@ public class PhoneNumberTest
 
         Assert.Throws<ArgumentException>(actual);
     }
+
+    [Fact]
+    public void GetFormattedPhoneNumberTest()
+    {
+        PhoneNumber testedPhoneNumber = new PhoneNumber("11984236577");
+        string expectedFormattedPhoneNumber = "(11) 98423-6577";
+
+        string formattedPhoneNumber = testedPhoneNumber.GetFormattedPhoneNumber();
+
+        Assert.Equal(expectedFormattedPhoneNumber, formattedPhoneNumber);
+    }
 }
