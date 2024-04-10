7f62c52913a5991625d7be0938062b199cf62712
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-10 16:56:02 -0300

    Creating SMTP settings

---
+++ paysys.webapi/Configuration/SmtpSettings.cs
@@ -0,0 +1,11 @@
+namespace paysys.webapi.Configuration;
+
+public class SmtpSettings
+{
+    public string? SmtpServer { get; set; }
+    public int Port { get; set; }
+    public string? SenderName { get; set; }
+    public string? SenderEmail { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+}
