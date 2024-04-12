6c1263c834c3c161597ffdab2d2fde5f9e30d51d
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-12 17:27:56 -0300

    Adding a SendMailWithTemplateAsync to MailInfraService

--- paysys.webapi/Infra/Mail/Service/IMailInfraService.cs
+++ paysys.webapi/Infra/Mail/Service/IMailInfraService.cs
@@ -1,6 +1,9 @@
+using paysys.webapi.Infra.Mail.Templates;
+
 namespace paysys.webapi.Infra.Mail.Service;
 
 public interface IMailInfraService
 {
     Task SendMailAsync(MailRequest request);
+    Task SendMailWithTemplateAsync(string receiverEmail, IMailTemplate mailTemplate);
 }
