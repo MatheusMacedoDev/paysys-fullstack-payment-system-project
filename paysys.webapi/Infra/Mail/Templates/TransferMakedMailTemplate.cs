ae193818010cd638e114bd89758743a5888e9352
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-16 21:17:53 -0300

    Adjusting transfer maked mail template to have transfer receiver specific message

--- paysys.webapi/Infra/Mail/Templates/TransferMakedMailTemplate.cs
+++ paysys.webapi/Infra/Mail/Templates/TransferMakedMailTemplate.cs
@@ -10,14 +10,18 @@ public class TransferMakedMailTemplate : IMailTemplate
     private readonly DateTime _transferDateTime;
     private readonly double _transferAmount;
 
+    private readonly TransferMailTo _transferMailTo;
+
     public string Subject { get => "Transferência realizada"; }
 
-    public TransferMakedMailTemplate(string transferSenderName, string transferReceiverName, DateTime transferDateTime, double transferAmount)
+    public TransferMakedMailTemplate(string transferSenderName, string transferReceiverName, DateTime transferDateTime, double transferAmount, TransferMailTo transferMailTo)
     {
         _transferSenderName = transferSenderName;
         _transferReceiverName = transferReceiverName;
         _transferDateTime = transferDateTime;
         _transferAmount = transferAmount;
+
+        _transferMailTo = transferMailTo;
     }
 
     public string GenerateEmailBody()
@@ -25,11 +29,10 @@ public class TransferMakedMailTemplate : IMailTemplate
         string source =
             @"<div style=""background-image: linear-gradient(#FFF, #EEF7F3); width: 100%; height: 100%; display: flex; flex-direction: column; padding: 16px"">
                 <h1 style=""color: #13423E; font-size: 24px"">
-                    Olá, {{TransferSenderName}}
+                    Olá, {{EmailReceiverName}}
                 </h1>
                 <p style=""color: #13423E; font-size: 18px"">
-                    Em {{TransferDate}}, foi realizado um pagamento da sua conta,
-                    no valor de {{TransferAmount}}. Os dados de destino são:
+                    {{TransferDescription}}
                 </p>
                 <ul style=""list-style-type: none"">
                     <li style=""color: #13423E; font-size: 16px"">
@@ -49,10 +52,31 @@ public class TransferMakedMailTemplate : IMailTemplate
 
         var template = Handlebars.Compile(source);
 
+        string emailReceiverName = "";
+        string transferDescription = "";
+
+        if (_transferMailTo == TransferMailTo.TransferSender)
+        {
+            emailReceiverName = _transferSenderName;
+            transferDescription = @$"
+                Em {{TransferDate}}, foi realizado um pagamento da sua conta,
+                no valor de {{TransferAmount}}. Os dados de destino são:
+            ";
+        }
+
+        if (_transferMailTo == TransferMailTo.TransferReceiver)
+        {
+            emailReceiverName = _transferReceiverName;
+            transferDescription = @$"
+                Em {{TransferDate}}, foi realizado um pagamento da para a sua conta,
+                no valor de {{TransferAmount}}. Os dados do pagamento são:
+            ";
+        }
+
         var data = new
         {
-            TransferSenderName = _transferSenderName,
-            TransferReceiverName = _transferReceiverName,
+            EmailReceiverName = emailReceiverName,
+            TransferDescription = transferDescription,
             TransferDate = _transferDateTime.Date.ToString("dd/MM/yyyy"),
             TransferDateTime = _transferDateTime.ToString(),
             TransferAmount = _transferAmount.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))
@@ -61,5 +85,11 @@ public class TransferMakedMailTemplate : IMailTemplate
         var result = template(data);
 
         return result;
+    }
+
+    public enum TransferMailTo
+    {
+        TransferSender,
+        TransferReceiver
     }
 }
