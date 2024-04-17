209c84b2e68373e1efc20d587303d14800f0b80b
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-17 18:22:04 -0300

    Refactoring the GenerateEmailBody method into TransferMakedMailTemplate

--- paysys.webapi/Infra/Mail/Templates/TransferMakedMailTemplate.cs
+++ paysys.webapi/Infra/Mail/Templates/TransferMakedMailTemplate.cs
@@ -28,40 +28,73 @@ public class TransferMakedMailTemplate : IMailTemplate
     {
         var handlebars = Handlebars.Create();
 
-        string emailReceiverName = "";
-        string otherPartName = "";
-        string transferDescriptionText = "";
+        string emailReceiverName = GetEmailReceiverName();
+
+        RegistrateOtherPartNameTemplate(handlebars);
+        RegistrateTransferDescriptionTemplate(handlebars);
+        RegistrateMainTemplate(handlebars);
+
+        return CompileTemplate(handlebars);
+    }
+
+    private string GetEmailReceiverName()
+    {
+        if (_transferMailTo == TransferMailTo.TransferSender)
+        {
+            return _transferSenderName;
+        }
+
+        return _transferReceiverName;
+    }
+
+    private void RegistrateOtherPartNameTemplate(IHandlebars handlebars)
+    {
+        string otherPartName;
 
         if (_transferMailTo == TransferMailTo.TransferSender)
         {
-            emailReceiverName = _transferSenderName;
             otherPartName = @"
                 <strong>Nome do recebedor:</strong>
                 {{TransferReceiverName}}
             ";
+        }
+        else
+        {
+            otherPartName = @"
+                <strong>Nome do pagador:</strong>
+                {{TransferSenderName}}
+            ";
+        }
+
+        handlebars.RegisterTemplate("Other_Part_Name_Template", otherPartName);
+    }
+
+    private void RegistrateTransferDescriptionTemplate(IHandlebars handlebars)
+    {
+        string transferDescriptionText = "";
+
+        if (_transferMailTo == TransferMailTo.TransferSender)
+        {
             transferDescriptionText = @"
                 Em {{TransferDate}}, foi realizado um pagamento da sua conta,
                 no valor de {{TransferAmount}}. Os dados de destino são:
             ";
         }
-
-        if (_transferMailTo == TransferMailTo.TransferReceiver)
+        else
         {
-            emailReceiverName = _transferReceiverName;
-            otherPartName = @"
-                <strong>Nome do pagador:</strong>
-                {{TransferSenderName}}
-            ";
             transferDescriptionText = @"
                 Em {{TransferDate}}, foi realizado um pagamento para a sua conta,
                 no valor de {{TransferAmount}}. Os dados do pagamento são:
             ";
+
         }
 
-        handlebars.RegisterTemplate("Other_Part_Name_Template", otherPartName);
         handlebars.RegisterTemplate("Transfer_Description_Text_Template", transferDescriptionText);
+    }
 
-        string source =
+    private void RegistrateMainTemplate(IHandlebars handlebars)
+    {
+        string transferMakedEmailMain =
             @"<div style=""background-image: linear-gradient(#FFF, #EEF7F3); width: 100%; height: 100%; display: flex; flex-direction: column; padding: 16px"">
                 <h1 style=""color: #13423E; font-size: 24px"">
                     Olá, {{EmailReceiverName}}
@@ -84,6 +117,16 @@ public class TransferMakedMailTemplate : IMailTemplate
                 </ul>
             </div>";
 
+        handlebars.RegisterTemplate("Transfer_Maked_Email_Template", transferMakedEmailMain);
+    }
+
+    private string CompileTemplate(IHandlebars handlebars)
+    {
+        string compilationResult = "";
+
+        string source = "{{>Transfer_Maked_Email_Template}}";
+        string emailReceiverName = GetEmailReceiverName();
+
         var template = handlebars.Compile(source);
 
         var data = new
@@ -96,14 +139,14 @@ public class TransferMakedMailTemplate : IMailTemplate
             TransferAmount = _transferAmount.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))
         };
 
-        var result = template(data);
+        compilationResult = template(data);
 
-        return result;
+        return compilationResult;
     }
+}
 
-    public enum TransferMailTo
-    {
-        TransferSender,
-        TransferReceiver
-    }
+public enum TransferMailTo
+{
+    TransferSender,
+    TransferReceiver
 }
