78f3938ef6d7e52e4de7373e12955f827c9abf3c
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-06 17:14:31 -0300

    Adding all the main colors of project to tailwind configuration

--- frontend/src/app/poc/page.tsx
+++ frontend/src/app/poc/page.tsx
@@ -1,7 +1,7 @@
 export default function pocPage() {
     return (
         <div className="w-screen h-screen">
-            <div className="h-28 w-28 bg-red-500"></div>
+            <div className="h-28 w-28 bg-gradient-to-t from-green-100 to-green-200"></div>
         </div>
     );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -6,7 +6,25 @@ const config: Config = {
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}'
     ],
-    theme: {},
+    theme: {
+        colors: {
+            green: {
+                100: '#0D2D2A',
+                200: '#13423E',
+                600: '#069A59',
+                700: '#2EBE7F',
+                800: '#63F5B5',
+                900: '#41E09A'
+            },
+            gray: {
+                500: '#CED0CC',
+                800: '#F3F7EE',
+                900: '#FFFFFF'
+            },
+            red: '#E04638'
+        },
+        extend: {}
+    },
     plugins: []
 };
 export default config;
