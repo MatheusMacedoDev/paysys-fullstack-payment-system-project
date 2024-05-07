a157e37fa3f79a8fd82519e457044376cfdc477f
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-07 13:43:09 -0300

    Presentation: Adding the rounded PaySys logo component to footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,9 +1,13 @@
+import PaySysLogoRounded from '../PaySysLogoRounded';
 import CopyrightText from './CopyrightText';
 
 export default function Footer() {
     return (
-        <div className="h-64 px-32 py-20 bg-gradient-to-t from-green-100 to-green-200">
-            <CopyrightText />
+        <div className="h-72 px-28 py-16 bg-gradient-to-t from-green-100 to-green-200">
+            <div className="h-full flex flex-col justify-between">
+                <PaySysLogoRounded />
+                <CopyrightText />
+            </div>
         </div>
     );
 }
