63b3c9bbaea617a2717734b71b771816b7706985
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-07 15:06:47 -0300

    Presentation: Creating a footer section with a title

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,13 +1,16 @@
 import PaySysLogoRounded from '../PaySysLogoRounded';
 import CopyrightText from './CopyrightText';
+import FooterSection from './FooterSection';
 
 export default function Footer() {
     return (
-        <div className="h-72 px-28 py-16 bg-gradient-to-t from-green-100 to-green-200">
+        <footer className="h-72 px-28 py-16 bg-gradient-to-t from-green-100 to-green-200 flex justify-between">
             <div className="h-full flex flex-col justify-between">
                 <PaySysLogoRounded />
                 <CopyrightText />
             </div>
-        </div>
+            <FooterSection sectionTitle="Redes Sociais" />
+            <FooterSection sectionTitle="Contatos" />
+        </footer>
     );
 }
