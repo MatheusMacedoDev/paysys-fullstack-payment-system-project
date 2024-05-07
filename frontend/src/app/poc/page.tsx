aa5c68c97d2f247dc012a32e44e20b3c0c48b13f
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-07 11:57:26 -0300

    Setting Raleway as default font in all frontend project

--- frontend/src/app/poc/page.tsx
+++ frontend/src/app/poc/page.tsx
@@ -2,6 +2,7 @@ export default function pocPage() {
     return (
         <div className="w-screen h-screen">
             <div className="h-28 w-28 bg-gradient-to-t from-green-100 to-green-200"></div>
+            <p className="text-xl font-extrabold tracking-wider">Raleway</p>
         </div>
     );
 }
