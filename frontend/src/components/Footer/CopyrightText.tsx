0116f163fce5d8218082ac692c9dc66fdf189988
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-07 13:12:45 -0300

    Presentation: Creating a copyright text into footer

---
+++ frontend/src/components/Footer/CopyrightText.tsx
@@ -0,0 +1,7 @@
+export default function CopyrightText() {
+    return (
+        <p className="text-xs font-light text-gray-800">
+            © Todos os direitos reservados - PaySys
+        </p>
+    );
+}
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import CopyrightText from './CopyrightText';
+
+export default function Footer() {
+    return (
+        <div className="h-64 px-32 py-20 bg-gradient-to-t from-green-100 to-green-200">
+            <CopyrightText />
+        </div>
+    );
+}
