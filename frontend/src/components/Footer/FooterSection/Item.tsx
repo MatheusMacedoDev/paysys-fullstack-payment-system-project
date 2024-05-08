8173cf4b5b4fae4aaa02c2b9448cb90304038b27
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-07 21:09:25 -0300

    Presentation: Adding remaining items to footer sections

--- frontend/src/components/Footer/FooterSection/Item.tsx
+++ frontend/src/components/Footer/FooterSection/Item.tsx
@@ -7,11 +7,11 @@ interface ItemProps {
 
 export default function Item({ icon, text }: ItemProps) {
     return (
-        <li className="flex gap-2">
+        <li className="flex gap-2 align-middle">
             <div className="rounded-full w-5 h-5 bg-gray-800 flex align-baseline justify-center">
                 {icon}
             </div>
-            <h4 className="font-light text-sm text-gray-800">{text}</h4>
+            <h4 className="font-light text-xs text-gray-800 mt-0.5">{text}</h4>
         </li>
     );
 }
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,7 +3,15 @@ import CopyrightText from './CopyrightText';
 import FooterSection from './FooterSection';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import {
+    faFacebookF,
+    faInstagram,
+    faTwitter,
+    faYoutube,
+    faWhatsapp
+} from '@fortawesome/free-brands-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 export default function Footer() {
     return (
@@ -15,19 +23,86 @@ export default function Footer() {
 
             <FooterSection.Container>
                 <FooterSection.Title>Redes Sociais</FooterSection.Title>
-                <FooterSection.Item
-                    icon={
-                        <FontAwesomeIcon
-                            className="text-green-100 w-2"
-                            icon={faFacebookF}
-                        />
-                    }
-                    text="PaymentSystem"
-                />
+                <FooterSection.ItemsWrapper>
+                    <FooterSection.Item
+                        icon={
+                            <FontAwesomeIcon
+                                className="text-green-100 w-2"
+                                icon={faFacebookF}
+                            />
+                        }
+                        text="PaymentSystem"
+                    />
+                    <FooterSection.Item
+                        icon={
+                            <FontAwesomeIcon
+                                className="text-green-100 w-3"
+                                icon={faInstagram}
+                            />
+                        }
+                        text="@PaySys"
+                    />
+                    <FooterSection.Item
+                        icon={
+                            <FontAwesomeIcon
+                                className="text-green-100 w-3"
+                                icon={faTwitter}
+                            />
+                        }
+                        text="PaySysOficial"
+                    />
+                    <FooterSection.Item
+                        icon={
+                            <FontAwesomeIcon
+                                className="text-green-100 w-3"
+                                icon={faYoutube}
+                            />
+                        }
+                        text="PaySystem"
+                    />
+                </FooterSection.ItemsWrapper>
             </FooterSection.Container>
 
             <FooterSection.Container>
                 <FooterSection.Title>Contato</FooterSection.Title>
+                <FooterSection.ItemsWrapper>
+                    <FooterSection.Item
+                        icon={
+                            <FontAwesomeIcon
+                                className="text-green-100 w-3 ml-px"
+                                icon={faWhatsapp}
+                            />
+                        }
+                        text="11 98775-4321"
+                    />
+                    <FooterSection.Item
+                        icon={
+                            <FontAwesomeIcon
+                                className="text-green-100 w-2.5 ml-px"
+                                icon={faPhone}
+                            />
+                        }
+                        text="4559-4321"
+                    />
+                    <FooterSection.Item
+                        icon={
+                            <FontAwesomeIcon
+                                className="text-green-100 w-3 ml-px"
+                                icon={faEnvelope}
+                            />
+                        }
+                        text="admin@paysys.com"
+                    />
+                    <FooterSection.Item
+                        icon={
+                            <FontAwesomeIcon
+                                className="text-green-100 w-3 ml-px"
+                                icon={faEnvelope}
+                            />
+                        }
+                        text="support@paysys.com"
+                    />
+                </FooterSection.ItemsWrapper>
             </FooterSection.Container>
         </footer>
     );
