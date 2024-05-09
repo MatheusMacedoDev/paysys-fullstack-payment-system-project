d54eb4219d46bfca35ed84ed7f1088a9bd8ce844
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-09 19:31:07 -0300

    Presentation: Adding responsivity to PaySysLogoRounded

--- frontend/src/components/PaySysLogoRounded/index.tsx
+++ frontend/src/components/PaySysLogoRounded/index.tsx
@@ -2,10 +2,11 @@ import Image from 'next/image';
 
 export default function PaySysLogoRounded() {
     return (
-        <div className="w-28 h-28 bg-gradient-to-t from-gray-500 to-gray-800 rounded-full flex align-center justify-center">
+        <div className="w-24 h-24 md:w-28 md:h-28 bg-gradient-to-t from-gray-500 to-gray-800 rounded-full flex align-center justify-center">
             <Image
                 src="paysys-logo.svg"
                 alt="Logomarca do PaySys"
+                className="w-14 md:w-16 h-14 m-auto"
                 width="70"
                 height="70"
             />
