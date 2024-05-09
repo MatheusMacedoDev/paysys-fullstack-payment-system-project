6fa9554239d8a66dab0e3b62a9ac1110f44d58c5
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-08 21:03:56 -0300

    Presentation: Refactoring Button component to segregate tailwind stylization

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,35 +1,21 @@
-import { twMerge } from 'tailwind-merge';
+import { ButtonColor, ButtonSize, getMergedStyle } from './styleOptions';
 
 interface ButtonProps {
     title: string;
-    buttonStyle?: 'light' | 'dark';
-    buttonSize?: 'small';
+    buttonColor?: ButtonColor;
+    buttonSize?: ButtonSize;
     className?: string;
     actionFn?: () => void;
 }
 
-const buttonFixedStyle = 'rounded-full';
-
-const buttonColorStyles = {
-    light: 'bg-gradient-to-b from-green-800 to-green-600 text-green-100',
-    dark: 'bg-gradient-to-b from-green-400 to-green-200 text-gray-900'
-};
-
-const buttonSizeStyles = {
-    small: 'w-32 h-10  font-bold text-sm'
-};
-
 export default function Button({
     title,
-    buttonStyle = 'light',
+    buttonColor = 'light',
     buttonSize = 'small',
     className,
     actionFn
 }: ButtonProps) {
-    const colorStyle = buttonColorStyles[buttonStyle];
-    const sizeStyle = buttonSizeStyles[buttonSize];
-
-    const style = twMerge(buttonFixedStyle, colorStyle, sizeStyle, className);
+    const style = getMergedStyle(buttonColor, buttonSize, className);
 
     return (
         <button type="button" onClick={actionFn} className={style}>
