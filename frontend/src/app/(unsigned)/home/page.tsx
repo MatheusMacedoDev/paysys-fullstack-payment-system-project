e39e71d917914dabe22232e2ade64c2458840b36
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-11 19:49:42 -0300

    Presentation: Adding home sections to home page

--- frontend/src/app/(unsigned)/home/page.tsx
+++ frontend/src/app/(unsigned)/home/page.tsx
@@ -1,3 +1,46 @@
+import Button from '@/components/Button';
+import HomeContainer from './HomeContainer';
+import HomeSection from './HomeSection';
+
 export default function Home() {
-    return <div>Home</div>;
+    return (
+        <HomeContainer>
+            <HomeSection.Container>
+                <HomeSection.ArticleGroup>
+                    <HomeSection.Heading>
+                        Faça pagamentos com simplicidade
+                    </HomeSection.Heading>
+                    <HomeSection.Description>
+                        Com o PaySys é possível fazer uma série de transações do
+                        dia-a-dia, com grande simplicidade, agilidade e
+                        transparência. Basta criar rapidamente uma conta fazer
+                        seu depósito e então poderá fazer transações com
+                        tranquilidade.
+                    </HomeSection.Description>
+                    <Button
+                        title="Criar Conta"
+                        buttonSize="medium"
+                        buttonColor="dark"
+                    />
+                </HomeSection.ArticleGroup>
+            </HomeSection.Container>
+            <HomeSection.Container>
+                <HomeSection.ArticleGroup>
+                    <HomeSection.Heading>
+                        Beneficios para lojistas
+                    </HomeSection.Heading>
+                    <HomeSection.Description>
+                        Lojistas, além de poder receber os pagamentos de seus
+                        clientes, ainda tem acesso a um dashboard exclusivo que
+                        permite verificar os dados de suas vendas.
+                    </HomeSection.Description>
+                    <Button
+                        title="Trabalhe Conosco"
+                        buttonSize="medium"
+                        buttonColor="dark"
+                    />
+                </HomeSection.ArticleGroup>
+            </HomeSection.Container>
+        </HomeContainer>
+    );
 }
