6cd7c53aad0a9020a11b1326709573042ba30dba
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-14 22:24:35 -0300

    Presentation: Adding an optional icon to Input form component

--- frontend/src/app/poc/page.tsx
+++ frontend/src/app/poc/page.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/Footer';
 import Form from '@/components/Form';
 import Header from '@/components/Header';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 export default function pocPage() {
     return (
@@ -8,10 +9,10 @@ export default function pocPage() {
             <Header />
 
             <div className="flex items-center justify-center py-20">
-                <Form.Container>
+                <Form.Container sendButtonTitle="Ação">
                     <Form.Title>Formulário</Form.Title>
                     <Form.InputsGroup>
-                        <Form.Input placeholder="Input 1" />
+                        <Form.Input placeholder="Input 1" icon={faUser} />
                         <Form.Input placeholder="Input 2" />
                         <Form.Input placeholder="Input 3" />
                     </Form.InputsGroup>
--- frontend/src/components/Form/Input.tsx
+++ frontend/src/components/Form/Input.tsx
@@ -1,12 +1,26 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import type { ComponentProps } from 'react';
 
-interface InputProps extends ComponentProps<'input'> {}
+interface InputProps extends ComponentProps<'input'> {
+    icon?: IconProp;
+}
 
-export default function Input({ ...rest }: InputProps) {
+export default function Input({ icon, ...rest }: InputProps) {
     return (
-        <input
-            {...rest}
-            className="border-green-300 border-2 rounded-full w-full h-16 outline-none px-8 text-xl font-light text-green-300"
-        />
+        <div className="relative">
+            <input
+                {...rest}
+                className="border-green-300 border-2 rounded-full w-full h-16 outline-none px-8 text-xl font-light text-green-300"
+            />
+            {icon && (
+                <div className="w-[50px] h-[50px] rounded-full bg-green-300 absolute right-[9px] top-[7px] flex items-center justify-center">
+                    <FontAwesomeIcon
+                        className="text-green-950 w-2/5"
+                        icon={icon}
+                    />
+                </div>
+            )}
+        </div>
     );
 }
