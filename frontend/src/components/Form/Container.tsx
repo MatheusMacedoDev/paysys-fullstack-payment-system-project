d8bf605afbd6f35724b593ba3c4e223c1b7e2e6b
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-14 22:23:49 -0300

    Presentation: Adding a submit button to form Container component

--- frontend/src/components/Form/Container.tsx
+++ frontend/src/components/Form/Container.tsx
@@ -1,17 +1,29 @@
 import { ReactNode } from 'react';
+import Button from '../Button';
 
 interface ContainerProps {
     children: ReactNode;
+    sendButtonTitle: string;
     onSubmit?: (data: object) => void;
 }
 
-export default function Container({ children, onSubmit }: ContainerProps) {
+export default function Container({
+    children,
+    sendButtonTitle,
+    onSubmit
+}: ContainerProps) {
     return (
         <form
             onSubmit={onSubmit}
             className="w-full 2xl:w-3/5 px-8 lg:px-14 py-12 lg:py-16 lg:shadow-[2px_2px_8px_0_rgba(0,0,0,0.2)] lg:rounded-xl bg-gray-900 flex flex-col items-center"
         >
             {children}
+            <Button
+                title={sendButtonTitle}
+                buttonColor="light"
+                buttonSize="big"
+                isSubmitButton={true}
+            />
         </form>
     );
 }
