0289ac78acc68684c335c7df820ca60fbd1175de
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-17 21:19:23 -0300

    Presentation: React Hook Form with Zod implemented into Login page

--- frontend/src/app/(unsigned)/login/page.tsx
+++ frontend/src/app/(unsigned)/login/page.tsx
@@ -1,16 +1,54 @@
+'use client';
+
 import Form from '@/components/Form';
 import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormProvider, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const makeLoginSchema = z.object({
+    email: z
+        .string()
+        .email('Formato de e-mail inválido.')
+        .min(1, 'O e-mail é obrigatório.'),
+    password: z.string().min(1, 'A senha é obrigatória.')
+});
+
+type MakeLoginData = z.infer<typeof makeLoginSchema>;
 
 export default function Login() {
+    const makeLoginForm = useForm<MakeLoginData>({
+        resolver: zodResolver(makeLoginSchema)
+    });
+
+    const { handleSubmit } = makeLoginForm;
+
+    function makeLogin(data: MakeLoginData) {
+        console.log(JSON.stringify(data));
+    }
+
     return (
         <Form.Background>
-            <Form.Container sendButtonTitle="Entrar">
-                <Form.Title>Entre na sua conta</Form.Title>
-                <Form.InputsGroup>
-                    <Form.Input placeholder="E-mail" icon={faEnvelope} />
-                    <Form.Input placeholder="Senha" icon={faKey} />
-                </Form.InputsGroup>
-            </Form.Container>
+            <FormProvider {...makeLoginForm}>
+                <Form.Container
+                    sendButtonTitle="Entrar"
+                    onSubmit={handleSubmit(makeLogin)}
+                >
+                    <Form.Title>Entre na sua conta</Form.Title>
+                    <Form.InputsGroup>
+                        <Form.Input
+                            name="email"
+                            placeholder="E-mail"
+                            icon={faEnvelope}
+                        />
+                        <Form.Input
+                            name="password"
+                            placeholder="Senha"
+                            icon={faKey}
+                        />
+                    </Form.InputsGroup>
+                </Form.Container>
+            </FormProvider>
         </Form.Background>
     );
 }
