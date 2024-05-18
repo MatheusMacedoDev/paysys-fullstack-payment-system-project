ab934b55aa4b6ebfcef679011c09857998503e83
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-17 21:17:57 -0300

    Presentation: Adapting Form component to basically work with React Hook Form

--- frontend/src/components/Form/Container.tsx
+++ frontend/src/components/Form/Container.tsx
@@ -1,21 +1,21 @@
 import { ReactNode } from 'react';
 import Button from '../Button';
+import type { ComponentProps } from 'react';
 
-interface ContainerProps {
+interface ContainerProps extends ComponentProps<'form'> {
     children: ReactNode;
     sendButtonTitle: string;
-    onSubmit?: (data: object) => void;
 }
 
 export default function Container({
     children,
     sendButtonTitle,
-    onSubmit
+    ...rest
 }: ContainerProps) {
     return (
         <form
-            onSubmit={onSubmit}
             className="w-full lg:w-3/5 xl:w-2/5 px-8 lg:px-14 xl:px-16 py-12 lg:py-16 xl:py-20 lg:shadow-[2px_2px_8px_0_rgba(0,0,0,0.2)] lg:rounded-xl bg-gray-900 flex flex-col items-center"
+            {...rest}
         >
             {children}
             <Button
--- frontend/src/components/Form/Input.tsx
+++ frontend/src/components/Form/Input.tsx
@@ -1,17 +1,23 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import type { ComponentProps } from 'react';
+import { useFormContext } from 'react-hook-form';
 
 interface InputProps extends ComponentProps<'input'> {
+    name: string;
     icon?: IconProp;
 }
 
-export default function Input({ icon, ...rest }: InputProps) {
+export default function Input({ name, icon, ...rest }: InputProps) {
+    const { register } = useFormContext();
+
     return (
         <div className="relative">
             <input
-                {...rest}
+                id={name}
                 className="border-green-300 border-2 rounded-full w-full h-16 lg:h-14 outline-none px-8 text-xl font-light text-green-300"
+                {...register(name)}
+                {...rest}
             />
             {icon && (
                 <div className="w-[50px] lg:w-[42px] h-[50px] lg:h-[42px] rounded-full bg-green-300 absolute right-[9px] top-[7px] flex items-center justify-center">
