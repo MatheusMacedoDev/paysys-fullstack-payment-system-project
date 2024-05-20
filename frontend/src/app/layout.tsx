b886a228c57a012e7289d8e1598d12758195656f
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-20 20:34:39 -0300

    Presentation: Font Awesome Icons configured for not add a default style

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from 'next';
 import { Raleway } from 'next/font/google';
 import './globals.css';
 
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 const raleway = Raleway({
     subsets: ['latin'],
     display: 'swap',
