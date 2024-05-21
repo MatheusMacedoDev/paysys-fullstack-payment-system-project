a6599a99eb3d65cd5fe5e29a2454f65b59e5ed52
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-21 20:13:13 -0300

    Presentation: Signed About page created

---
+++ frontend/src/app/signed/(pages)/about/page.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import { DataCard } from '../../components/DataCard';
+
+export default function AboutPage() {
+    return (
+        <div className="flex flex-col items-center h-screen p-6 lg:p-10">
+            <Image
+                src="/undraw-team-spirit.svg"
+                alt="Equipe reunida demonstrando o espírito de equipe."
+                className="w-full sm:w-[400px] mt-20 lg:mt-28 mb-16"
+                width={338}
+                height={228}
+            />
+
+            <DataCard.BigCard
+                title="Sobre o PaySys"
+                description={`
+                    PaySys é um sistema de pagamento unificado que atende tanto B2B
+                    - empresa pra empresa - quanto B2C - empresa para consumidor.`}
+            />
+
+            <Image
+                src="/undraw-my-answer.svg"
+                alt="Pessoa observando as respostas para as suas dúvidas."
+                className="w-full sm:w-[400px] mt-36 mb-16"
+                width={325}
+                height={252}
+            />
+
+            <DataCard.SmallCardGroup className="pb-28">
+                <DataCard.SmallCard
+                    title="Simplicidade"
+                    description="Com uma interface de usuário fácil de entender."
+                />
+                <DataCard.SmallCard
+                    title="Transparência"
+                    description="As tranferências são registradas e podem ser auditadas."
+                />
+                <DataCard.SmallCard
+                    title="Facilidade"
+                    description="Tudo pode ser feito em poucos passos."
+                />
+            </DataCard.SmallCardGroup>
+        </div>
+    );
+}
