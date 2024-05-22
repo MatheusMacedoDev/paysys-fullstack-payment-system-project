57315df4f21bed140d7768181c48e57b80190529
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-22 20:26:39 -0300

    Presentation: Default classname added to form Container

--- frontend/src/components/Form/Container.tsx
+++ frontend/src/components/Form/Container.tsx
@@ -1,22 +1,26 @@
 import { ReactNode } from 'react';
 import Button from '../Button';
 import type { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface ContainerProps extends ComponentProps<'form'> {
     children: ReactNode;
     sendButtonTitle: string;
+    className?: string;
 }
 
 export default function Container({
     children,
     sendButtonTitle,
+    className,
     ...rest
 }: ContainerProps) {
+    const containerDefaultStyle =
+        'w-full lg:w-3/5 xl:w-2/5 px-8 lg:px-14 xl:px-16 py-12 lg:py-16 xl:py-20 lg:shadow-[2px_2px_8px_0_rgba(0,0,0,0.2)] lg:rounded-xl bg-gray-900 flex flex-col items-center';
+    const containerStyle = twMerge(containerDefaultStyle, className);
+
     return (
-        <form
-            className="w-full lg:w-3/5 xl:w-2/5 px-8 lg:px-14 xl:px-16 py-12 lg:py-16 xl:py-20 lg:shadow-[2px_2px_8px_0_rgba(0,0,0,0.2)] lg:rounded-xl bg-gray-900 flex flex-col items-center"
-            {...rest}
-        >
+        <form className={containerStyle} {...rest}>
             {children}
             <Button
                 title={sendButtonTitle}
