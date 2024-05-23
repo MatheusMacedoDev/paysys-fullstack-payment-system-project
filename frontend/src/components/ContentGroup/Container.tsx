0e9d9599659b2722188309c8bfc360e5ffa85e67
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-23 15:42:47 -0300

    Presentation: Content component added to ContentGroup father component

--- frontend/src/components/ContentGroup/Container.tsx
+++ frontend/src/components/ContentGroup/Container.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 
 export default function Container({ children, className }: ContainerProps) {
     const containerDefaultStyle =
-        'w-full lg:w-7/12 py-20 lg:py-16 lg:px-24 rounded-3xl lg:shadow-xl flex flex-col items-center gap-y-16';
+        'w-full lg:w-7/12 py-20 lg:py-16 lg:px-24 rounded-3xl lg:shadow-xl flex flex-col items-center gap-y-12';
     const containerStyle = twMerge(containerDefaultStyle, className);
 
     return <div className={containerStyle}>{children}</div>;
--- frontend/src/components/ContentGroup/index.ts
+++ frontend/src/components/ContentGroup/index.ts
@@ -1,7 +1,9 @@
 import Container from './Container';
+import Content from './Content';
 import Title from './Title';
 
 export const ContentGroup = {
     Container,
+    Content,
     Title
 };
