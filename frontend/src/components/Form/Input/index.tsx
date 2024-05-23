f522b9f262a97c839d796fe5c5abe938cba9ffea
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-22 21:04:19 -0300

    Presentation: ClassName prop added to form Input component

--- frontend/src/components/Form/Input/index.tsx
+++ frontend/src/components/Form/Input/index.tsx
@@ -3,23 +3,33 @@ import type { InputHTMLAttributes } from 'react';
 import InputIcon from './InputIcon';
 import InputMask from 'react-input-mask';
 import { useFormContext } from 'react-hook-form';
+import { twMerge } from 'tailwind-merge';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     name: string;
     mask?: string;
+    className?: string;
     icon?: IconProp;
 }
 
-export default function Input({ name, mask, icon, ...rest }: InputProps) {
+export default function Input({
+    name,
+    mask,
+    className,
+    icon,
+    ...rest
+}: InputProps) {
     const { register } = useFormContext();
 
     const formProps = register(name);
 
-    const inputStyle = `
+    const inputDefaultStyle = `
         border-green-300 border-2 rounded-full
         w-full h-16 lg:h-14 outline-none px-8
         text-xl font-light text-green-300
     `;
+
+    const inputStyle = twMerge(inputDefaultStyle, className);
 
     return (
         <div className="relative flex flex-1">
