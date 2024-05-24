56580c3da8a1d6f002bdede96fe903a4b511b9ba
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-24 18:20:57 -0300

    Presentation: ContentGroup Container component added resposivity

--- frontend/src/components/ContentGroup/Container.tsx
+++ frontend/src/components/ContentGroup/Container.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 
 export default function Container({ children, className }: ContainerProps) {
     const containerDefaultStyle =
-        'w-full lg:w-7/12 py-20 lg:py-16 lg:px-24 rounded-3xl flex flex-col items-center gap-y-12 lg:shadow-[3px_3px_10px_0_rgba(0,0,0,0.2)]';
+        'w-full lg:w-10/12 xl:w-7/12 py-20 lg:py-16 lg:px-24 rounded-3xl flex flex-col items-center gap-y-12 lg:shadow-[3px_3px_10px_0_rgba(0,0,0,0.2)]';
     const containerStyle = twMerge(containerDefaultStyle, className);
 
     return <div className={containerStyle}>{children}</div>;
